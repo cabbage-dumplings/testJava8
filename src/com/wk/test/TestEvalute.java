dcf726a1cc6d3b3836608fee14df0f22371c94f8
cabbage-dumplings <384201386@qq.com>
2020-07-31 22:04:16 +0800

    第五次提交

--- src/com/wk/test/TestEvalute.java
+++ src/com/wk/test/TestEvalute.java
@@ -17,6 +17,9 @@ public class TestEvalute {
             System.out.println(person.getAge()+" "+person.getSalary()+" "+person.getUsername());
         }
         System.out.println("aa");
+        System.out.println("bb");
+        System.out.println("cc");
+        System.out.println("dd");
         int a = 1;
         int b = 2;
         System.out.println(add(a,b));
