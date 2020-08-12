d772b9c99974d8704d32f6bff93f01e7abdb829a
cabbage-dumplings <384201386@qq.com>
2020-08-12 21:54:02 +0800

    提交TestAnnotation

---
+++ src/com/wk/test/TestAnnotation.java
@@ -0,0 +1,38 @@
+package com.wk.test;
+
+import org.junit.Test;
+
+/**
+ * @author WK
+ * @create 2020-08-12-21:12
+ */
+public class TestAnnotation {
+    /**
+     *
+     */
+    @Test
+    public void test1(){
+
+    }
+    /**
+     *
+     */
+    @Test
+    public void test2(){
+
+    }
+    /**
+     *
+     */
+    @Test
+    public void test3(){
+
+    }
+    /**
+     *
+     */
+    @Test
+    public void test4(){
+
+    }
+}
